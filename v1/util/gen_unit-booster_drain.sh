8898fda38d3dca48e9e62bd9a2293b63470ec4e7
Chris Fortescue <fortescu@adobe.com>
2016-09-01 15:15:57 +0000

    Make the initial booster-drain state launched

--- v1/util/gen_unit-booster_drain.sh
+++ v1/util/gen_unit-booster_drain.sh
@@ -5,9 +5,9 @@
 #
 #
 
-LOCALPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BINPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-source $LOCALPATH/drain_helpers.sh
+source $BINPATH/../lib/drain_helpers.sh
 
 assert_root
 unit_name=booster-draining-$(cat /etc/machine-id)-$(date +%s).service
@@ -17,17 +17,14 @@ json=/tmp/test.json
 cat <<EOF > $json
 {
     "name": "${unit_name}",
-    "desiredState": "loaded",
+    "desiredState": "launched",
     "options": [
         { "section": "Unit", "name": "Description", "value": "Draining Launched by Booster"},
         { "section": "Service", "name": "Type", "value": "oneshot"},
         { "section": "Service", "name": "User", "value": "root"},
-        { "section": "Service", "name": "RemainAfterExit", "value": "no"},
-        { "section": "Service", "name": "RestartSec", "value": "5"},
+        { "section": "Service", "name": "RemainAfterExit", "value": "yes"},
         { "section": "Service", "name": "StandardOutput", "value": "journal+console"},
-        { "section": "Service", "name": "ExecStartPre", "value": "/bin/bash -cx 'test -z \"\$(/home/core/ethos-systemd/v1/util/lockctl.sh host_state)\"'"},
-        { "section": "Service", "name": "ExecStart", "value": "/bin/bash -xc '/home/core/ethos-systemd/v1/util/drain.sh drain BOOSTER'"},
-        { "section": "Service", "name": "ExecStartPost", "value": "/bin/bash -cx 'test -z \"\$(/home/core/ethos-systemd/v1/util/lockctl.sh host_state)\" || /home/core/ethos-systemd/v1/util/lockctl.sh unlock_host BOOSTER'"},
+        { "section": "Service", "name": "ExecStart", "value": "/bin/bash -xc '/home/core/ethos-systemd/v1/util/booster-drain.sh --notify mock'"},
         { "section": "X-Fleet", "name": "MachineID", "value": "$(cat /etc/machine-id)"}
     ]
 }
@@ -35,19 +32,36 @@ EOF
 curl -v --unix-socket /var/run/fleet.sock -H 'Content-Type: application/json' -X PUT -d@$json http:/fleet/fleet/v1/units/${unit_name}
 
 >&2 cat << EOF
-Useful followups:
+Useful followups sans fleetctl:
 
  - Start
 sudo curl -v --unix-socket /var/run/fleet.sock -H 'Content-Type: application/json' -X PUT -d'{"desiredState": "launched"}'  http://fleet/fleet/v1/units/${unit_name}
+
  - Status
 sudo curl -vs --unix-socket /var/run/fleet.sock  http:/fleet/fleet/v1/state?machineID=`cat /etc/machine-id` | jq '.states[]'
 
  - Delete unit
 sudo curl -v --unix-socket /var/run/fleet.sock  -X DELETE http:/fleet/fleet/v1/units/${unit_name}
+
  - Find show unit names
 sudo curl -vs --unix-socket /var/run/fleet.sock  http:/fleet/fleet/v1/units  | jq -r '.units[]|.name'
+
  - Find booster units
 sudo curl -vs --unix-socket /var/run/fleet.sock  http:/fleet/fleet/v1/units  | jq '.units[]|select(.name | contains("booster-draining"))|.'
+
+ - logs
+sudo journalctl -u ${unit_name} 
+
+# Cleanup
+
+for i in $(sudo curl -s --unix-socket /var/run/fleet.sock  http:/fleet/fleet/v1/units  | jq -r '.units[]|select(.name | contains("booster-draining"))|.name'); do sudo curl -v --unix-socket /var/run/fleet.sock  -X DELETE http:/fleet/fleet/v1/units/$i;done
+
+# Debugging
+Use 2 shells. Replace all the above --unix-sockets /var/run/fake.sock.  
+
+toolbox:
+  dnf install -y socat tcpdump
+  socat -v unix-listen:/media/root/var/run/fake.sock,fork unix-connect:/media/root/var/run/fleet.sock
 
 -f4tq 
 EOF
